fbbfb5aeaeefeaad3f1009c423a5b785afd57372
Duyan Kim <arbolkim@gmail.com>
2022-04-18 00:12:44 +0900

    feat: 고객 정보가 입력된 배열을 csv 파일에 넣고, 만 20세 이하 고객 수 카운트

---
+++ src/write/writeCustData.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+writeFile = (data) =>  {
+    fs.writeFileSync("src/result/custData.csv", data.toString());
+}
+
+readFile = () => {
+    console.log("***********");
+    let tooYoungCust = [];
+    const custData = fs.readFileSync("src/result/custData.csv", "utf-8").toString().split("\n");
+    for(let i = 1; i < custData.length-1; i++) {
+        let birthDate = custData[i].split(" ")[1];
+        console.log(`${birthDate}`.substring(0,4));
+        if (`${birthDate}`.substring(0,4) * 1 > 2002) {
+            tooYoungCust.push(i);
+        }
+    }
+    return tooYoungCust;
+}
+
+module.exports = { writeFile, readFile };
