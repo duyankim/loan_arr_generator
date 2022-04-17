b6ef866106cd810423c7626fcd8ab640ac5b2663
Duyan Kim <arbolkim@gmail.com>
2022-04-17 18:23:38 +0900

    Initial commit for arr generator

--- src/main/java/org/food/delivery/domain/order/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package org.food.delivery.domain.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
