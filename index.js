077f8453ee6dde31b73f02c9ce1ab02533547b6f
Duyan Kim <arbolkim@gmail.com>
2022-04-21 23:59:03 +0900

    feat: 사용자 입력 로직 구현

--- index.js
+++ index.js
@@ -17,54 +17,35 @@ rl.question("고객을 몇 명 생성하시겠습니까?: ", (input) => {
 		custData = getCust();
 	}
 	console.log(`${input}명 생성되었습니다.`);
-	rl.close();
+	// rl.close();
 
 	writeFile(custData);
-	let tooYoungCust = readFile();
-	let moreCustCnt = tooYoungCust.length;
+	// let tooYoungCust = readFile();
+	// let moreCustCnt = tooYoungCust.length;
 
-	for (let j = tooYoungCust.length - 1; j >= 0; j--) {
-        custData.splice(tooYoungCust[j], 1);
-	}
+	// for (let j = tooYoungCust.length - 1; j >= 0; j--) {
+    //     custData.splice(tooYoungCust[j], 1);
+	// }
 
-	for (let k = 0; k < moreCustCnt; k++) {
-		custData.push(getCust());
-	}
+	// for (let k = 0; k < moreCustCnt; k++) {
+	// 	custData.push(getCust());
+	// }
 	
-	console.log(`${moreCustCnt}명 더 생성되었습니다.\n`);
+	// console.log(`${moreCustCnt}명 더 생성되었습니다.\n`);
 
 	getTxDt();
 });
 
-let promptQuestion = {
-	"arrTxDt" : "계약일은 언제입니까? YYYYMMDD 형태로 써주세요.",
-	"lnLmtAmt" : "한도액은 얼마입니까?",
-	"dsbsTxDt" : "차입일은 언제입니까? YYYYMMDD 형태로 써주세요.",
-	"dsbsAmt" : "차입액은 얼마입니까?"
-}
-
 getTxDt = () => {
-	console.log(`${promptQuestion.arrTxDt}`);
+	console.log("계약일(YYYYMMDD) 한도액 차입일(YYYYMMDD) 차입액");
 	let inputData = []; //계약일, 한도액, 차입일, 차입액
-
-	rl.setPrompt("> ")
-	rl.prompt()
 
 	rl.on("line", function(line) {
 		inputData.push(line);
-
-		if (inputData.length == 1) {
-			for (seq in promptQuestion) {
-				console.log(`${promptQuestion.seq}`);
-				rl.prompt();
-				inputData.push(line);
-			}
-			rl.close();
-		}
-		
-		})
+		console.log(inputData);
+		rl.close();
+	})
 	rl.on("close", function() {
 		process.exit()
 	})
-	console.log(inputData);
 }
